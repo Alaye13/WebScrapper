7e68654a12160fd62ce729fe548440f0cc9b74b1
Alaye13 <ifennaekwenem@gmail.com>
2023-12-19 15:14:17 -0500

    Comments

--- src/Scraper.ts
+++ src/Scraper.ts
@@ -7,7 +7,7 @@ async function scrapeWebsite(url: string): Promise<string> {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
 
-    // Your scraping logic goes here
+    // Scraping logic
     const paragraphs = $('p'); // Select all paragraphs, adjust the selector as needed
 
     let pageContent = '';
@@ -15,14 +15,13 @@ async function scrapeWebsite(url: string): Promise<string> {
       pageContent += $(element).text() + '\n';
     });
 
-    return pageContent.trim(); // Return the contents as a string, removing leading/trailing whitespace
+    return pageContent.trim(); // Return the contents as a string, as well as removing leading/trailing whitespace
   } catch (error) {
     console.error('Error:', (error as any).message); // Note: 'any' used for simplicity
     return ''; // Return an empty string in case of an error
   }
 }
 
-// Replace 'https://example.com' with the URL you want to scrape
 const targetUrl = 'http://ufcstats.com/fighter-details/45f0cc9d18f35137';
 scrapeWebsite(targetUrl)
   .then((content) => {
