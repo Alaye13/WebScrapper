e7f4d54a5c46faf768cc9d43ca059d2dbb3c8bf1
Alaye13 <ifennaekwenem@gmail.com>
2023-12-19 15:45:21 -0500

    Implementations of some changes in run method

--- src/Scraper.ts
+++ src/Scraper.ts
@@ -6,42 +6,60 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 import * as createCsvWriter from 'csv-writer';
 
+class WebScraper {
+  private url: string;
 
-async function scrapeWebsite(url: string): Promise<string> {
-  try {
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
+  constructor(url: string) {
+    this.url = url;
+  }
 
-    // Scraping logic
-    const paragraphs = $('p'); 
-    // Select all paragraphs, adjust the selector as needed
-   
-    let pageContent = '';
-    paragraphs.each((index, element) => {
-      pageContent += $(element).text() + '\n';
+  private async scrapeWebsite(): Promise<string> {
+    try {
+      const response = await axios.get(this.url);
+      const $ = cheerio.load(response.data);
+
+      //Scraping Logic
+      const paragraphs = $('p'); // Select all paragraphs, adjust the selector as needed
+
+      let pageContent = '';
+      paragraphs.each((index, element) => {
+        pageContent += $(element).text() + '\n';
+      });
+
+      return pageContent.trim(); // Return the contents as a string, removing leading/trailing whitespace
+    } catch (error) {
+      console.error('Error:', (error as any).message); // Note: 'any' used for simplicity
+      return ''; // Return an empty string in case of an error
+    }
+  }
+
+  // Save comtents from the terminal (PArsed Information) into a csv data file
+  private saveToCsv(data: string, fileName: string): void {
+    const csvWriter = createCsvWriter.createObjectCsvWriter({
+      path: fileName,
+      header: [{ id: 'content', title: 'Content' }],
     });
 
-    return pageContent.trim(); // Return the contents as a string, as well as removing whitespace
-  } catch (error) {
-    console.error('Error:', (error as any).message); // Note: 'any' used for simplicity
-    return ''; // Return an empty string in case of an error
+    csvWriter.writeRecords([{ content: data }]);
   }
+
+  // Run Logic
+  public async run(): Promise<void> {
+    try {
+      const content = await this.scrapeWebsite();
+      console.log('Page Content:\n', content);
+  
+      // Replace 'output.csv' with your desired CSV file name and path
+      this.saveToCsv(content, 'output.csv');
+      console.log('Data saved to CSV file.');
+    } catch (error: any) {
+      console.error('Error:', error.message);
+    }
+  }
+  
 }
 
-function saveToCsv(data: string, fileName: string): void {
-  const csvWriter = createCsvWriter.createObjectCsvWriter({
-    path: fileName,
-    header: [{ id: 'content', title: 'Content' }],
-  });
-
-  csvWriter.writeRecords([{ content: data }]);
-}
-
+// Usage
 const targetUrl = 'http://ufcstats.com/fight-details/f9ec8bedc15ece93';
-scrapeWebsite(targetUrl)
-  .then((content) => {
-    console.log('Page Content:\n', content);
-  })
-  .catch((error) => {
-    console.error('Error:', error.message);
-  });
+const scraper = new WebScraper(targetUrl);
+scraper.run();
