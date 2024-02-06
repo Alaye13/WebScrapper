125d6f7b64e4df5c618a2f969a9fcce36c58aa38
Alaye13 <ifennaekwenem@gmail.com>
2024-02-06 14:10:39 -0500

    changes

---
+++ dist/Scraper.js
@@ -0,0 +1,36 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = require("axios");
+const cheerio = require("cheerio");
+function scrapeWebsite(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield axios_1.default.get(url);
+            console.log('Response data:', response.data); // Log response data to verify HTML content
+            const $ = cheerio.load(response.data);
+            const $selected = $('p.b-fight-details__table-body');
+            console.log($selected);
+        }
+        catch (error) {
+            console.error('Error:', error.message); // Note: 'any' used for simplicity
+        }
+    });
+}
+// Replace 'https://example.com' with the URL you want to scrape
+const targetUrl = 'http://ufcstats.com/fight-details/a74a8c1e0a49070d';
+scrapeWebsite(targetUrl)
+    .then(() => {
+    console.log('Scraping complete.');
+})
+    .catch((error) => {
+    console.error('Error:', error.message);
+});
