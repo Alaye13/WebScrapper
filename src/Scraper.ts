a2a5ff3eec1cb9bac99b3b59b55b084e2456fad0
Alaye13 <ifennaekwenem@gmail.com>
2024-02-06 13:03:47 -0500

    Msg

---
+++ src/Scraper.ts
@@ -0,0 +1,11 @@
+/**
+ * Ifenna Ekwenem
+ * Web Scraper Logic
+ */
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import * as createCsvWriter from 'csv-writer';
+import * as fs from 'fs';
+
+
+
